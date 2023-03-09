a87ad73f7262f0201383eab98d619be1dc63f95c
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-09 08:58:59 +0300

    working with API ruby

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'net/http'
 require 'open-uri'
 require 'json'
@@ -6,13 +7,24 @@ class GetPrograms
 
   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
-  def get_programs
+  def get_response_body
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
   end
 
+  def program_school
+    # we use the JSON library to parse the API response into nicely formatted JSON
+      programs = JSON.parse(self.get_response_body)
+      programs.collect do |program|
+        program["agency"]
+      end
+    end
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_response_body
+# puts programs
+
+programs = GetPrograms.new
+puts programs.program_school.uniq
+# binding.pry
